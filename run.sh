aad7763650af9b28dd173dfc845abc15171139f7
Gerry Weißbach <gerry.w@gammaproduction.de>
2015-08-13 13:50:12 +0200

    Remove not needed output. Tag.

--- run.sh
+++ run.sh
@@ -10,16 +10,16 @@ cd phpxref-0.7.1 && perl ./phpxref.pl && cd ..
 # Prepare
 DOCUMENT_BASE="DokuWiki.docset/Contents/Resources"
 
+# Delete create
 rm -rf $DOCUMENT_BASE
 mkdir -p $DOCUMENT_BASE
 cp -a ./output "$DOCUMENT_BASE/Documents"
 
-echo $PATH
+# Modify Path
 export PATH=`echo $PATH | sed -e 's/:\.\/[^:]*//g'`
-echo $PATH
 
 echo "Renaming to lowercase with:"
-find "$DOCUMENT_BASE/Documents/" -name "*[A-Z]*" -execdir rename -fv "y/A-Z/a-z/" "{}" \;
+find "$DOCUMENT_BASE/Documents/" -name "*[A-Z]*" -execdir rename -fv "y/A-Z/a-z/" "{}" \; 2>&1 > /dev/null
 echo "DONE. (Renaming to lowercase)"
 
 # Generate the Docset
@@ -33,3 +33,4 @@ rm -rf phpxref-0.7.1* output dokuwiki
 
 # Show everything
 ls -altr *
+du -sh *
